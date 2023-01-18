3708e8611300529909d5227df67d725c192caf1a
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-01-18 22:54:46 +0530

    Angular Material added to package json

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [],
+  exports: [MatSidenavModule],
 })
 export class MaterialModule {}
