3a158365ed08c7d15a5bb5accd1a061a0dad38e5
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-01-18 22:36:52 +0530

    Material module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, HeaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
