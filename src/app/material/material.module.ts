64cd7191ea48c2c4d894e11444a21538cb5f6d64
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-01-19 14:40:33 +0530

    top header items added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
-let MATERIAL_MODULES = [MatSidenavModule, MatToolbarModule];
+let MATERIAL_MODULES = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSelectModule,
+];
 
 @NgModule({
   declarations: [],
