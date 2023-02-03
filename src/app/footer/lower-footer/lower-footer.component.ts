27b7d8998ebed34f041f97acd95fcadeacc22e4b
AmanKumarVerma <amakv055@gmail.com>
2023-02-03 16:06:19 +0530

    lower footer added

---
+++ src/app/footer/lower-footer/lower-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lower-footer',
+  templateUrl: './lower-footer.component.html',
+  styleUrls: ['./lower-footer.component.sass']
+})
+export class LowerFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
