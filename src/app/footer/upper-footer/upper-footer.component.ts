719483d166c80c0ef1d9be20f0d7217db57f28bc
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-02-04 19:00:59 +0530

    upper footer basic

--- src/app/footer/upper-footer/upper-footer.component.ts
+++ src/app/footer/upper-footer/upper-footer.component.ts
@@ -3,13 +3,70 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upper-footer',
   templateUrl: './upper-footer.component.html',
-  styleUrls: ['./upper-footer.component.sass']
+  styleUrls: ['./upper-footer.component.sass'],
 })
 export class UpperFooterComponent implements OnInit {
+  dropdown: boolean = false;
 
-  constructor() { }
+  aboutUs: string[] = [
+    'About Us',
+    'Careers',
+    'Press Releases',
+    'Amazon Science',
+  ];
 
-  ngOnInit(): void {
-  }
+  connectWithUs: string[] = ['Facebook', 'Twitter', 'Instagram'];
 
+  makeMoneyWithUs: string[] = [
+    'Sell on Amazon',
+    'Sell under Amazon Accelerator',
+    'Protect and Build Your Brand',
+    'Amazon Global Selling',
+    'Become an Affiliate',
+    'Fulfilment by Amazon',
+    'Advertise Your Products',
+    'Amazon Pay on Merchants',
+  ];
+
+  letUsHelpYou: string[] = [
+    'COVID-19 and Amazon',
+    'Your Account',
+    'Returns Centre',
+    '100% Purchase Protection',
+    'Amazon App Download',
+    'Amazon Assistant Download',
+    'Help',
+  ];
+
+  countries: string[] = [
+    'Australia',
+    'Brazil',
+    'Canada',
+    'China',
+    'France',
+    'Germany',
+    'Italy',
+    'Japan',
+    'Mexico',
+    'Netherlands',
+    'Poland',
+    'Singapore',
+    'Spain',
+    'Turkey',
+    'United Arab Emirates',
+    'United Kingdom',
+    'United States',
+  ];
+
+  upperFooterContent: { [key: string]: string[] } = {
+    'Get to Know Us': this.aboutUs,
+    'Connect with Us': this.connectWithUs,
+    'Make Money with Us': this.makeMoneyWithUs,
+    'Let Us Help You': this.letUsHelpYou,
+  };
+  entries = Object.entries(this.upperFooterContent);
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
