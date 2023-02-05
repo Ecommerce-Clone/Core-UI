73990a48f979573b0f720baa776a917c475e98e0
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-02-05 14:50:15 +0530

    lint issues fixed

--- src/app/body/product-category-block/product-category-block.component.ts
+++ src/app/body/product-category-block/product-category-block.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-category-block',
   templateUrl: './product-category-block.component.html',
-  styleUrls: ['./product-category-block.component.sass']
+  styleUrls: ['./product-category-block.component.sass'],
 })
-export class ProductCategoryBlockComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ProductCategoryBlockComponent {}
