5ce7ce151c4e43d0bcd465bebab9f105de580187
AmanKumarVerma <amakv055@gmail.com>
2023-02-05 12:20:30 +0530

    body module added

---
+++ src/app/body/product-category-block/product-category-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-category-block',
+  templateUrl: './product-category-block.component.html',
+  styleUrls: ['./product-category-block.component.sass']
+})
+export class ProductCategoryBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
